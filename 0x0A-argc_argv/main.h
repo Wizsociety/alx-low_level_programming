2915df6010e58f8136bd1310861cda5217e89c30
Wizsociety <iankimathi14@gmail.com>
2023-11-21 12:56:38 +0300

    Args and argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(__attribute__((unused)) int argc, char *argv[]);
+int main(__attribute__((unused))int argc, __attribute__((unused))char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
