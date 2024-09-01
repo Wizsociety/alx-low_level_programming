dd7bf2ff4dbb3f2830fbe6c47462ff4bca05949b
Wizsociety <iankimathi14@gmail.com>
2024-09-01 10:52:01 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
+
 /**
- * main is entry point
- *
- * puts is used to display output
- *
- * return 0 on success
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
